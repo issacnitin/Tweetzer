4117eefd02e2530b383502a0b2315710df3f6225
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-03-02 23:36:12 +0530

    Fixing some API calls stuff

--- frontend/src/Utils/Network/AuthenticationAPI.ts
+++ frontend/src/Utils/Network/AuthenticationAPI.ts
@@ -7,28 +7,28 @@ class AuthenticationAPI extends BaseAPI {
 
     async signIn(username: string, password: string) {
         let response = await this.sendRequest("/api/v1/profile/login", "POST", { "username": username, "password": password})
-        let responseJSON = await response.json();
+        let responseBody = await response.json();
         return {
-            body: responseJSON.body.json(),
-            statusCode: responseJSON.status
+            body: responseBody,
+            statusCode: response.status
         }
     }
 
     async signUp(username: string, email: string, password: string) {
         let response = await this.sendRequest("/api/v1/profile/register", "POST", { "username": username, "email": email, "password": password})
-        let responseJSON = await response.body.json();
+        let responseBody = await response.json();
         return {
-            body: responseJSON.body.json(),
-            statusCode: responseJSON.status
+            body: responseBody,
+            statusCode: response.status
         }
     }
 
     async refreshToken() {
         let response = await this.sendRequest("/api/v1/profile/refreshtoken", "GET")
-        let responseJSON = await response.json();
+        let responseBody = await response.json();
         return {
-            body: responseJSON.body.json(),
-            statusCode: responseJSON.status
+            body: responseBody,
+            statusCode: response.status
         }
     }
 }
