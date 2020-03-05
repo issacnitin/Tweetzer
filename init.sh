78ff4662871da5e1aa1bfbcf2ad14a929382ee73
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-03-05 22:13:43 +0530

    Containerized build for nodejs

--- init.sh
+++ init.sh
@@ -1,11 +1,7 @@
 #! /usr/bin/sudo
 
-cd frontend
-npm install
-npm run-script build
-cd ../backend
+cd backend
 docker-compose up -d --build
-
 sudo echo "127.0.0.1 tweetzer.com www.tweetzer.com" >> /etc/hosts
 echo "Open browser, go to tweetzer.com"
 echo "Please delete the entry from /etc/hosts"
