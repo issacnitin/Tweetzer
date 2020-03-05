861656af77bcb17e3d287c320ec8b198a41dcdff
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-03-05 23:16:04 +0530

    Coloring output

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/sudo
 
+RED='\033[0;35m'
+NC='\033[0m' # No Color
+
 sudo rm -rf ./temp/
 cd backend
-docker-compose up -d --build --force-recreate
 sudo echo "127.0.0.1 tweetzer.com www.tweetzer.com" >> /etc/hosts
-echo "Open browser, go to tweetzer.com"
-echo "Don't forget to delete the entry from /etc/hosts"
+printf "${RED}Open browser, go to tweetzer.com once the docker-compose logs has stabilized\n"
+printf "Don't forget to delete the entry from /etc/hosts\n${NC}"
+docker-compose up -d --build --force-recreate
