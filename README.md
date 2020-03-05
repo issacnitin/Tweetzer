73282a139543e2121bb22eefc54b7b7fdf7fcada
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-03-05 23:25:05 +0530

    Should I add rate limiter?

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 To start: 
 1. Run init.sh as sudo (sudo sh init.sh)
 2. tweetzer.com in your browser
+
+FAQ:
+Nginx throws 403 error
+    - Wait for 5 minutes, for some time nginx container won't have access to the filesystem as nodejs container is building in nginx's path
--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 RED='\033[0;35m'
 NC='\033[0m' # No Color
 
-sudo rm -rf ./temp/
+git clean -xdf
 cd backend
 sudo echo "127.0.0.1 tweetzer.com www.tweetzer.com" >> /etc/hosts
-printf "${RED}Open browser, go to tweetzer.com once the docker-compose logs has stabilized\n"
+printf "${RED}Open browser, go to tweetzer.com 5 minutes after the docker-compose logs has stabilized (I'm building frontend in docker)\n"
 printf "Don't forget to delete the entry from /etc/hosts\n${NC}"
 docker-compose up -d --build --force-recreate
