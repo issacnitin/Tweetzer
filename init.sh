80e7e1f61545af8e77729366bc414511cc0b18ed
Nitin Issac Joy <nitin.i.joy@gmail.com>
2020-03-05 20:00:14 +0530

    Run init.sh as sudo

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ cd frontend
 npm install
 npm run-script build
 cd ../backend
-docker-compose up --build
+docker-compose up -d --build
 
-echo "127.0.0.1 tweetzer.com www.tweetzer.com" >> /etc/hosts
-
+sudo echo "127.0.0.1 tweetzer.com www.tweetzer.com" >> /etc/hosts
+echo "Open browser, go to tweetzer.com"
 echo "Please delete the entry from /etc/hosts"
